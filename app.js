975e163a727ce352b413d05271d55a05c23adb5e
HanamDavid <GiwmHanam@protonmail.com>
2024-04-24 19:35:54 -0500

    update

--- app.js
+++ app.js
@@ -11,39 +11,21 @@ const observer = new IntersectionObserver(
         });
 
 });
-const vh= window.innerHeight*3.5
+
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
 
 const tracker = document.getElementById("tracker");
 const pointer = document.getElementById("pointer");
+
 window.onpointermove = event =>{
     const{pageX, pageY} = event;
-    if(pageY<vh){
 
         pointer.animate(
             {
                 left: `${pageX}px`,
                 top: `${pageY}px`
             }   , { duration: 1, fill: "forwards" });
-
-        tracker.animate(
-            {
-                left: `${pageX}px`,
-                top: `${pageY}px`
-            }   , { duration: 3000, fill: "forwards" }); 
-    }else{
-        pointer.animate(
-            {
-                left: `${pageX}px`,
-            }   , { duration: 1, fill: "forwards" });
-
-        tracker.animate(
-            {
-                left: `${pageX}px`,
-            }   , { duration: 3000, fill: "forwards" }); 
-    }
-
 
 }
 
